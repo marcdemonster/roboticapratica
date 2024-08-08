962839d249d00ed820da9dd2b9a52b4e3c907ebd
Marcos Roberto <marcostec2013@hotmail.com>
2024-08-08 19:53:14 -0300

    modificado os arquivos para teste

--- Aprendendo C++/main.c
+++ Aprendendo C++/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Olá Galera");
+int main(){
+    printf("Olï¿½ Galera");
 }
--- Aprendendo C++/projeto.cpp
+++ Aprendendo C++/projeto.cpp
@@ -1,8 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main(){
-    cout << "Ola, Mundo!" << endl;
-    return 0;
-}

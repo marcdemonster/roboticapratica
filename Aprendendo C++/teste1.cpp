8d4efa7a4ad053725a4f38b8b16cae8422a35234
Marcos Roberto <marcostec2013@hotmail.com>
2024-08-08 13:05:35 -0300

    Revisao da linguagem C++

---
+++ Aprendendo C++/teste1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+         cout << "Hello world"<<endl;
+         cout <<"I am programmer"<< endl;
+
+         cout<<"Hello world\n";
+         cout<<"I am programmer";
+         
+         return 0;
+    }
